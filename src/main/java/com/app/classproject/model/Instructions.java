cedf4a50136f86c6cac029b963e64e3bc0f96681
qqqyyy111 <55216735+qqqyyy111@users.noreply.github.com>
2019-10-07 15:38:10 -0400

    Update Instructions.java

--- src/main/java/com/app/classproject/model/Instructions.java
+++ src/main/java/com/app/classproject/model/Instructions.java
@@ -10,6 +10,31 @@ public class Instructions {
     public static final int[] LDX_opc = {1, 0, 1, 0, 0, 1};
     public static final int[] STX_opc = {1, 0, 1, 0, 1, 0};
 
+ // part 2
+		public static final int[] JZ_opc  = {0, 0, 1, 0, 1, 0};
+	    public static final int[] JNE_opc = {0, 0, 1, 0, 1, 1};
+	    public static final int[] JCC_opc = {0, 0, 1, 1, 0, 0};
+	    public static final int[] JMA_opc = {0, 0, 1, 1, 0, 1};
+	    public static final int[] JSR_opc = {0, 0, 1, 1, 1, 0};
+	    public static final int[] RFS_opc = {0, 0, 1, 1, 1, 1};
+	    public static final int[] SOB_opc = {0, 1, 0, 0, 0, 0};
+	    public static final int[] JGE_opc = {0, 1, 0, 0, 0, 1};
+	    public static final int[] AMR_opc = {0, 0, 0, 1, 0, 0};
+	    public static final int[] SMR_opc = {0, 0, 0, 1, 0, 1};
+	    public static final int[] AIR_opc = {0, 0, 0, 1, 1, 0};
+	    public static final int[] SIR_opc = {0, 0, 0, 1, 1, 1};
+	    public static final int[] MLT_opc = {0, 1, 0, 1, 0, 0};
+	    public static final int[] DVD_opc = {0, 1, 0, 1, 0, 1};
+	    public static final int[] TRR_opc = {0, 1, 0, 1, 1, 0};
+	    public static final int[] AND_opc = {0, 1, 0, 1, 1, 1};
+	    public static final int[] ORR_opc = {0, 1, 1, 0, 0, 0};
+	    public static final int[] NOT_opc = {0, 1, 1, 0, 0, 1};
+	    public static final int[] SRC_opc = {0, 1, 1, 1, 1, 1};
+	    public static final int[] RRC_opc = {1, 0, 0, 0, 0, 0};
+	    public static final int[] IN_opc  = {1, 1, 1, 1, 0, 1};
+	    public static final int[] OUT_opc = {1, 1, 1, 1, 1, 0};
+	    public static final int[] CHK_opc = {1, 1, 1, 1, 1, 1};
+	    
     public Computer computer;
     public memory instruction = new memory();
 
@@ -37,6 +62,22 @@ public class Instructions {
                 return LDX();
             case 42:
                 return STX();
+            case 10:
+            	return JZ();
+            case 11:
+            	return JNE();
+            case 12:
+            	return JCC();
+            case 13:
+            	return JMA();
+            case 14:
+            	return JSR();
+            case 15:
+            	return RFS();
+            case 16:
+            	return SOB();
+            case 17:
+            	return JGE();
             default:
                 return Computer.ERROR_RET_CODE;
         }
@@ -239,6 +280,459 @@ public class Instructions {
         }
     }
 
+     /**
+     * part 2, transfer instruction
+    */
+    
+    /**
+     *  Jump if Zero
+     */
+    
+   public int JZ(){
+	   int EA = getEffectiveAdr();	
+	   int tempR = getValueFromRById(instruction.gpr);
+	   if(tempR == 0) {
+	              computer.pc.setValue(EA);
+	   }else {
+				  computer.pc.setValue(computer.pc.getBase10Value() + 1);
+   }
+	   return Computer.SUCCESS_RET_CODE;
+  }
+   
+  
+
+/**
+    *  Jump if not Equal
+    */
+   
+   public int JNE() {
+	   int EA = getEffectiveAdr();
+	   int tempR = getValueFromRById(instruction.gpr);
+	   if(tempR != 0) {
+           computer.pc.setValue(EA);
+	   	}else {
+			  computer.pc.setValue(computer.pc.getBase10Value() + 1);
+	   	}
+	   	return Computer.SUCCESS_RET_CODE;
+   }
+  
+   /**
+    *  Jump If Condition Code cc replaces r for this instruction
+    */
+   
+   public int JCC() {
+	   int EA = getEffectiveAdr();
+	   switch(instruction.ccr) {
+	   case 0:
+		   if(computer.ccr[0].getValue()[instruction.ccr] == 1){
+			   computer.pc.setValue(EA);
+		   }else {
+		   computer.pc.setValue(computer.pc.getBase10Value() + 1);
+	   	}
+		   return Computer.SUCCESS_RET_CODE;
+	   case 1:
+		   if(computer.ccr[1].getValue()[instruction.ccr] == 1){
+			   computer.pc.setValue(EA);
+		   }else {
+		   computer.pc.setValue(computer.pc.getBase10Value() + 1);
+	   	}
+		   return Computer.SUCCESS_RET_CODE;
+	   case 2:
+		   if(computer.ccr[2].getValue()[instruction.ccr] == 1){
+			   computer.pc.setValue(EA);
+		   }else {
+		   computer.pc.setValue(computer.pc.getBase10Value() + 1);
+	   	}
+		   return Computer.SUCCESS_RET_CODE;
+	   case 3:
+		   if(computer.ccr[3].getValue()[instruction.ccr] == 1){
+			   computer.pc.setValue(EA);
+		   }else {
+		   computer.pc.setValue(computer.pc.getBase10Value() + 1);
+	   		}
+		   return Computer.SUCCESS_RET_CODE;
+		   default:
+			   System.out.println("Error");
+               return Computer.ERROR_RET_CODE;
+	   }
+	  
+  }
+   
+   /** 
+    *  Unconditional Jump To Address
+    *  r is ignored in this instruction
+    */
+   
+   public int JMA() {
+	   int EA = getEffectiveAdr();
+	   computer.pc.setValue(EA);
+	   
+	return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Jump and Save Return Address
+    *  R0 should contain pointer to arguments
+    *  Argument list should end with –1 (all 1s) value
+    */
+   
+   public int JSR() {
+	   int EA = getEffectiveAdr();
+	   computer.gpr[3].setValue(computer.pc.getBase10Value() + 1);
+	   computer.pc.setValue(EA);
+	   
+	  return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Return From Subroutine w/ return code 
+    *  as Immed portion (optional) stored in the instruction’s address field. 
+    *  IX, I fields are ignored.
+    */
+   
+   public int RFS(int immed) {
+	   computer.gpr[0].setValue(immed);
+	   computer.pc.setValue(computer.gpr[3].getValue());
+	   return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Subtract One and Branch
+    */
+   
+   public int SOB() {
+	   int EA = getEffectiveAdr();
+	   switch(instruction.gpr) {
+	   case 0:
+		   computer.gpr[0].setValue(computer.gpr[0].getBase10Value()-1);
+		   if(computer.gpr[0].getBase10Value()>0)
+			   computer.pc.setValue(EA);
+		   break;
+	   case 1:
+		   computer.gpr[1].setValue(computer.gpr[1].getBase10Value()-1);
+		   if(computer.gpr[1].getBase10Value()>0)
+			   computer.pc.setValue(EA);
+		   break;
+	   case 2:
+		   computer.gpr[2].setValue(computer.gpr[2].getBase10Value()-1);
+		   if(computer.gpr[2].getBase10Value()>0)
+			   computer.pc.setValue(EA);
+		   break;
+	   case 3:
+		   computer.gpr[3].setValue(computer.gpr[3].getBase10Value()-1);
+		   if(computer.gpr[3].getBase10Value()>0)
+			   computer.pc.setValue(EA);
+		  
+	   default:
+			   System.out.println("Error");
+			   return Computer.ERROR_RET_CODE;
+	   }
+	   return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Jump Greater Than or Equal To
+    */
+   
+   public int JGE() {
+	   int EA = getEffectiveAdr();
+	   switch(instruction.gpr) {
+	   case 0:
+		   if(computer.gpr[0].getBase10Value() >= 0)
+			   computer.pc.setValue(EA);
+		   break;
+	   case 1:
+		   if(computer.gpr[1].getBase10Value() >= 0)
+			   computer.pc.setValue(EA);
+		   break;	
+	   case 2:
+		   if(computer.gpr[2].getBase10Value() >= 0)
+			   computer.pc.setValue(EA);
+		   break;
+	   case 3:
+		   if(computer.gpr[3].getBase10Value() >= 0)
+			   computer.pc.setValue(EA);
+		   break;
+		   default:
+			   System.out.println("Error");
+			   return Computer.ERROR_RET_CODE;
+	   }
+	   return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Add Memory To Register
+    */
+   public int AMR() {
+	   int EA = getEffectiveAdr();
+	   computer.mar.setValue(EA);
+       computer.mbr.setValue(computer.RAM[EA].MEM);
+       computer.pc.setValue(computer.pc.getBase10Value() + 1);
+       computer.ir.setValue(computer.RAM[computer.pc.getBase10Value()].MEM);
+	   switch(instruction.gpr) {
+	   case 0:
+		   computer.gpr[0].setValue(computer.gpr[0].getBase10Value() + computer.RAM[EA].mem);
+		   break;              
+	   case 1:
+		   computer.gpr[1].setValue(computer.gpr[1].getBase10Value() + computer.RAM[EA].mem);
+		   break;
+	   case 2:
+		   computer.gpr[2].setValue(computer.gpr[2].getBase10Value() + computer.RAM[EA].mem);
+	   break;
+	   case 3:
+		   computer.gpr[3].setValue(computer.gpr[3].getBase10Value() + computer.RAM[EA].mem);
+	   break;
+	   default:
+		   System.out.println("Error");
+		   return Computer.ERROR_RET_CODE;
+	   }
+	   return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Subtract Memory From Register
+    */
+   public int SMR() {
+	   int EA = getEffectiveAdr();
+	   computer.mar.setValue(EA);
+       computer.mbr.setValue(computer.RAM[EA].MEM);
+       computer.pc.setValue(computer.pc.getBase10Value() + 1);
+       computer.ir.setValue(computer.RAM[computer.pc.getBase10Value()].MEM);
+       switch(instruction.gpr) {
+       case 0: 
+    	   computer.gpr[0].setValue(computer.gpr[0].getBase10Value() - computer.RAM[EA].mem);
+    	   break;
+       case 1:
+    	   computer.gpr[1].setValue(computer.gpr[1].getBase10Value() - computer.RAM[EA].mem);
+    	   break;
+       case 2:
+    	   computer.gpr[2].setValue(computer.gpr[2].getBase10Value() - computer.RAM[EA].mem);
+    	   break;
+       case 3:
+    	   computer.gpr[3].setValue(computer.gpr[3].getBase10Value() - computer.RAM[EA].mem);
+    	   break;
+    	   default:
+    		   System.out.println("Error");
+    		   return Computer.ERROR_RET_CODE;
+       }
+       return Computer.SUCCESS_RET_CODE;
+   }
+   
+   /** 
+    *  Add  Immediate to Register
+    *  IX and I are ignored in this instruction
+    */
+   	public int AIR(int immed) {
+   	 int EA = getEffectiveAdr();
+	 computer.mar.setValue(EA);
+     computer.mbr.setValue(computer.RAM[EA].MEM);
+     computer.pc.setValue(computer.pc.getBase10Value() + 1);
+     computer.ir.setValue(computer.RAM[computer.pc.getBase10Value()].MEM);
+   
+    	 switch(instruction.gpr) {
+    		case 0:
+    			computer.gpr[0].setValue(computer.gpr[0].getBase10Value() + immed);
+    			if(computer.gpr[0].getBase10Value() == 0) {
+    		    	 computer.gpr[0].setValue(immed);
+    		     }
+    			break;
+    		case 1:
+    			computer.gpr[1].setValue(computer.gpr[1].getBase10Value() + immed);
+    			if(computer.gpr[1].getBase10Value() == 0) {
+   		    	 	computer.gpr[1].setValue(immed);
+   		     }
+    			break;
+    		case 2:
+    			computer.gpr[2].setValue(computer.gpr[2].getBase10Value() + immed);
+    			if(computer.gpr[2].getBase10Value() == 0) {
+   		    	 	computer.gpr[2].setValue(immed);
+   		     }
+    			break;
+    		case 3:
+    			computer.gpr[3].setValue(computer.gpr[3].getBase10Value() + immed);
+    			if(computer.gpr[3].getBase10Value() == 0) {
+   		    	 	computer.gpr[3].setValue(immed);
+   		     }
+    			break;
+    			default:
+    				System.out.println("Error");
+      		    return Computer.ERROR_RET_CODE;
+    		}
+    	 return Computer.SUCCESS_RET_CODE;
+     }
+   	  
+   
+   
+   /** 
+    *  Subtract  Immediate  from Register
+    */
+   
+   	public int SIR(int immed) {
+   		int EA = getEffectiveAdr();
+   		computer.mar.setValue(EA);
+        computer.mbr.setValue(computer.RAM[EA].MEM);
+        computer.pc.setValue(computer.pc.getBase10Value() + 1);
+        computer.ir.setValue(computer.RAM[computer.pc.getBase10Value()].MEM);
+   		switch(instruction.gpr) {
+   		case 0:
+   			computer.gpr[0].setValue(computer.gpr[0].getBase10Value() - immed);
+   			if(computer.gpr[0].getBase10Value() == 0) {
+		    	 computer.gpr[0].setValue(-immed);
+		     }
+   			break;
+   		case 1:
+   			computer.gpr[1].setValue(computer.gpr[1].getBase10Value() - immed);
+   			if(computer.gpr[1].getBase10Value() == 0) {
+		    	 computer.gpr[1].setValue(-immed);
+		     }
+   			break;
+   		case 2:
+   			computer.gpr[2].setValue(computer.gpr[2].getBase10Value() - immed);
+   			if(computer.gpr[2].getBase10Value() == 0) {
+		    	 computer.gpr[2].setValue(-immed);
+		     }
+   			break;
+   		case 3:
+   			computer.gpr[3].setValue(computer.gpr[3].getBase10Value() - immed);
+   			if(computer.gpr[3].getBase10Value() == 0) {
+		    	 computer.gpr[3].setValue(-immed);
+		     }
+   			break;
+   			default:
+   				System.out.println("Error");
+     		    return Computer.ERROR_RET_CODE;
+   		}
+   		return Computer.SUCCESS_RET_CODE;
+    }
+   /** 
+    *  Multiply Register by Register
+    */
+   
+   /** 
+    *  Divide Register by Register
+    */
+   
+   /** 
+    *  Test the Equality of Register and Register
+    */
+   public int TRR() {
+	   int temp1 = getValueFromRById(instruction.rx);
+	   int temp2 = getValueFromRById(instruction.ry);
+	   if(temp1 == temp2) {
+		   computer.ccr[4].setValue(4);
+	   }else {
+		   computer.ccr[4].setValue(0);
+	   }
+	   return Computer.SUCCESS_RET_CODE;
+   }
+	   
+   /** 
+    *  Logical And of Register and Register
+    */
+   	public int AND() {
+   		int temp1 = getValueFromRById(instruction.rx);
+   		int temp2 = getValueFromRById(instruction.ry);
+   		
+   		setValueToRById(instruction.rx, temp1 & temp2);
+   		return Computer.SUCCESS_RET_CODE;
+   	}
+   
+   
+    /** 
+     *  Logical Or of Register and Register
+     */
+   	public int ORR() {
+   		int temp1 = getValueFromRById(instruction.rx);
+   		int temp2 = getValueFromRById(instruction.ry);
+   		
+   		setValueToRById(instruction.rx, temp1 | temp2);
+   		return Computer.SUCCESS_RET_CODE;
+   	}
+   	
+   	
+    /** 
+     *  Logical Not of Register To Register
+     */
+   	public int NOT() {
+   		int temp1 = getValueFromRById(instruction.rx);
+   		setValueToRById(instruction.rx, ~temp1);
+   		return Computer.SUCCESS_RET_CODE;
+   	}
+   	
+   	
+    /** 
+     *  Shift Register by Count
+     */
+   	public int SRC() {
+   		int tempR = getValueFromRById(instruction.gpr);
+   		if(instruction.lr == 1){
+   			this.setValueToRById(instruction.gpr, tempR << instruction.count);
+   		}
+   		if(instruction.lr == 0) {
+   			this.setValueToRById(instruction.gpr, tempR >> instruction.count);
+   		}
+   		return Computer.SUCCESS_RET_CODE;
+   	}
+   	
+  
+   	/** 
+     *  Rotate Register by Count
+     */
+   
+   	/** 
+     *  Input Character To Register from Device
+     */
+   	
+   	/** 
+     *  Output Character to Device from Register
+     */
+   	
+   	/** 
+     *  Check Device Status to Register
+     */
+
+ // get value by ID from general register R0-R3
+    public int getValueFromRById(int id) {
+    	int temp = 0;
+    	switch (id) {
+    		case 0:
+    			temp = computer.gpr[0].getBase10Value();
+    			break;
+    		case 1:
+    			temp = computer.gpr[1].getBase10Value();
+    			break;
+    		case 2:
+    			temp = computer.gpr[2].getBase10Value();
+    			break;
+    		case 3:
+    			temp = computer.gpr[3].getBase10Value();
+    			break;
+    		default:
+    			System.out.println("Error");
+    			break;
+    	}
+    	return temp;
+    }
+    
+    // set value by ID to general register R0-R3
+    public void setValueToRById(int id, int value) {
+    	switch(id) {
+    	case 0:
+    		computer.gpr[0].setValue(value);
+    	case 1:
+    		computer.gpr[1].setValue(value);
+    	case 2:
+    		computer.gpr[2].setValue(value);
+    	case 3:
+    		computer.gpr[3].setValue(value);
+    		default:
+    			System.out.println("Error");
+		break;
+    	}
+    }
+    
+    
+    
     public void printInfo() {
 
     }
